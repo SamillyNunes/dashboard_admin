83d846f17e946e266aa84676dd61bc545540fa40
Samilly <samillynunes19@gmail.com>
2024-05-23 14:18:07 -0300

    ui: working on layout style

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,12 +10,14 @@ interface LayoutProps{
 
 export default function Layout(props: LayoutProps){
     return (
-        <div>
+        <div className={`flex h-screen w-screen`} >
             <SideMenu />
-            <Head title={props.title} subtitle={props.subtitle} />
-            <Content>
-                {props.children}
-            </Content>
+            <div className={`flex flex-col w-full p-7 bg-gray-300`} >
+                <Head title={props.title} subtitle={props.subtitle} />
+                <Content>
+                    {props.children}
+                </Content>
+            </div>
         </div>
     );
 }
