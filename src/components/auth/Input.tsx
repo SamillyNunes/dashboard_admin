9f1cd36d306e4f43498d6fac4c68e7e81fefce66
Samilly <samillynunes19@gmail.com>
2024-05-28 10:59:49 -0300

    ui: working on auth layout

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -9,13 +9,18 @@ interface InputProps{
 
 export default function Input(props: InputProps){
     return props.notRender ? null : (
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4">
             <label> {props.label} </label>
             <input 
                 type={props.type} 
                 required={props.required}
                 value={props.value}
                 onChange={e => props.onValueChanged?.(e.target.value)}
+                className="
+                    px-4 py-3 rounded-lg bg-gray-200 mt-2
+                    border focus:border-blue-500
+                    focus:outline-none focus:bg-white
+                "
             />
         </div>
     );
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -7,23 +7,51 @@ export default function Auth(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    function submit(){
+        if(mode==='login'){
+            console.log('Login');
+        }else {
+            console.log('Cadastrar');
+        }
+    }
+
     return (
-        <div>
-            <h1>Autenticação</h1>
-            <Input
-                label="Email"
-                value={email}
-                type="email"
-                onValueChanged={setEmail}
-                required
-            />
-            <Input
-                label="Senha"
-                value={password}
-                type="password"
-                onValueChanged={setPassword}
-                required
-            />
+        <div className="flex flex-col h-screen items-center justify-center">
+            <div className="w-1/2">
+                <h1 className="text-xl font-bold mb-5">
+                    {mode === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na Plataforma'}
+                </h1>
+                <Input 
+                    label="Email"
+                    value={email}
+                    type="email"
+                    onValueChanged={setEmail}
+                    required
+                />
+                <Input
+                    label="Senha"
+                    value={password}
+                    type="password"
+                    onValueChanged={setPassword}
+                    required
+                />
+
+                <button onClick={submit} className="
+                    w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg
+                    px-4 py-3 mt-6
+                ">
+                    {mode === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+
+                <hr className="my-6 border-gray-300 w-full" />
+
+                <button onClick={submit} className="
+                    w-full bg-red-500 hover:bg-red-400 text-white rounded-lg
+                    px-4 py-3 
+                ">
+                    Entrar com Google
+                </button>
+            </div>
         </div>
     );
 }
